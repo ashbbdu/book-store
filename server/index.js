3bb55c7499d4dbe3e972e64c456672145968421b
Ashish Srivastava <ashishsrivastava@Ashishs-MacBook-Pro.local>
2023-10-11 23:01:12 +0530

    wip

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { connect } = require("./config/database");
+// const authRoutes = require("./routes/Auth") 
+const app = express();
+
+const PORT = process.env.PORT || 5000 
+require("dotenv").config()
+app.use(express.json());
+
+// function to connect the DB
+connect();
+
+// Routes
+// app.use("/api/v1/auth" , authRoutes)
+
+app.get("/" , (req , res) => {
+    res.send("App is running")
+})
+
+app.listen(PORT , () => {
+    console.log(`App is running on PORT : ${PORT}`)
+})
