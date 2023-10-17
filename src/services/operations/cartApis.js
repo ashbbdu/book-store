389851f756d4fc0b4865872ae73125814ebe20b2
Ashish Srivastava <ashishsrivastava@Ashishs-MacBook-Pro.local>
2023-10-17 20:55:17 +0530

    cart

--- src/services/operations/cartApis.js
+++ src/services/operations/cartApis.js
@@ -6,36 +6,37 @@ import { cartEndpoints } from "../apis";
 import apiServices from "../apiServices";
 
 export const getCartData = () => {
-    return async (dispatch) => {
+  return async (dispatch) => {
     //   dispatch(setLoading(true));
-      try {
-        const response = await apiServices.httpGet(cartEndpoints.CART_DATA_API);
-        console.log(response.cartItem , "item")
-       response.cartItem.map(res => {
-        console.log(res , "res")
+    try {
+      const response = await apiServices.httpGet(cartEndpoints.CART_DATA_API);
+      console.log(response.cartItem, "item");
+      response.cartItem.map((res) => {
+        console.log(res, "res");
         dispatch(setCartData(res.book));
-       })
-    
        
-  
-      } catch (error) {
-        toast.error("Something went wrong");
-      }
+      });
+    
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
     //   dispatch(setLoading(false));
-    };
   };
+};
 
-export const addToCart = (id , data) => {
-    return async (dispatch) => {
-      dispatch(setLoading(true));
-      try {
-        const response = await apiServices.httpPost(cartEndpoints.ADD_TO_CART_API , {bookId : id});
-        console.log(response , "response")
-        dispatch(setCartData(data));
-  
-      } catch (error) {
-        toast.error("Something went wrong");
-      }
-      dispatch(setLoading(false));
-    };
+export const addToCart = (id, data) => {
+  return async (dispatch) => {
+    dispatch(setLoading(true));
+    try {
+      const response = await apiServices.httpPost(
+        cartEndpoints.ADD_TO_CART_API,
+        { bookId: id }
+      );
+      console.log(response, "response");
+      dispatch(setCartData(data));
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+    dispatch(setLoading(false));
   };
+};
